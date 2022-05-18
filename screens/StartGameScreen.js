3cddc78f7b1e2118b7157580b9a75f8a7b08e7bf
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-05-18 16:03:03 -0700

    added styling to input container and explored some props for react native core components

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,40 @@
-import {TextInput, View} from "react-native"
+import { StyleSheet, TextInput, View } from "react-native"
+
+import PrimaryButton from "../components/PrimaryButton"
 
 function StartGameScreen() {
   return(
-    <View>
-      <TextInput />
-      BUTTONS
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   )
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: "#72063c",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 4 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+})
