2f8ce3b00249a26b25db7d0b3daa546bfe423d48
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-05-18 17:43:26 -0700

    formating and implementing expo-linear-gradient

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {LinearGradient} from "expo-linear-gradient"
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
       <StatusBar style="auto" />
       <StartGameScreen/>
-    </>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  }
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,9 +5,15 @@ import PrimaryButton from "../components/PrimaryButton"
 function StartGameScreen() {
   return (
       <View style={styles.inputContainer}>
-        <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+          </View>
+        </View>
       </View>
   )
 }
@@ -16,6 +22,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
@@ -36,5 +44,12 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center"
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  buttonContainer: {
+    flex: 1
   }
 })
